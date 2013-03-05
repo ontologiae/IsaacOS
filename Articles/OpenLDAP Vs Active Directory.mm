e300109ecb1f9b3c1270495650ed0d60aa07a155
Pierre-Alex <ontologiae@gmail.com>
2013-03-05 14:55:46 +0100

    First commit

---
+++ Articles/OpenLDAP Vs Active Directory.mm
@@ -0,0 +1,137 @@
+<map version="0.8.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1224433923648" ID="Freemind_Link_580348333" MODIFIED="1224434186593" TEXT="OpenLDAP Vs Active Directory">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1224433984904" ID="_" MODIFIED="1224434186553" POSITION="right" TEXT="Exemple fondateur de NT4">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1224433999407" ID="Freemind_Link_1868114442" MODIFIED="1224434186555" TEXT="Yavait quoi avant">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1224434007663" ID="Freemind_Link_1212466155" MODIFIED="1224434186558" TEXT="keske &#xe7;a a apport&#xe9;">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1224434051688" ID="Freemind_Link_382448622" MODIFIED="1224434186561" TEXT="Pouvoir cr&#xe9;er un mail facilement tout en cr&#xe9;ant un utilisateur sur le r&#xe9;seau">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1224434068705" ID="Freemind_Link_813530499" MODIFIED="1224434186563" TEXT="G&#xe9;rer un partage d&apos;imprimante">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1224434075393" ID="Freemind_Link_1970902109" MODIFIED="1224434186569" TEXT="G&#xe9;rer des droits relativement facilement">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1224434089585" ID="Freemind_Link_1787052188" MODIFIED="1224434186579" TEXT="Point cl&#xe9; : fonctionnalit&#xe9; qui doivent &#xea;ytre accessible, &#xe0; un ing&#xe9;nieur g&#xe9;n&#xe9;raliste ou sp&#xe9;cialis&#xe9; dans un autre que l&apos;info">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1224434128363" ID="Freemind_Link_228425790" MODIFIED="1224434186590" TEXT="Typique Jean-Emile">
+<node COLOR="#111111" CREATED="1224434135171" ID="Freemind_Link_443321403" MODIFIED="1224434186591" TEXT="Croit que l&apos;info &#xe7;a marche comme la physique"/>
+<node COLOR="#111111" CREATED="1224434155540" ID="Freemind_Link_1864538117" MODIFIED="1224434186593" TEXT="&#xe7;a marchait hier, pourquoi &#xe7;a marche pas aujourd&apos;hui"/>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1224434278591" ID="Freemind_Link_28422712" MODIFIED="1224434283475" POSITION="right" TEXT="El&#xe9;ment de contexte">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1224434220215" ID="Freemind_Link_1433969405" MODIFIED="1224434285863" TEXT="LIen poste client chez le particulier et chez l&apos;&#xe9;tudiant pour s&apos;imposer en entreprise">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1224434256087" ID="Freemind_Link_1179863674" MODIFIED="1224434285863" TEXT="Arriv&#xe9;e du PC dans la classe moyenne">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1224434466159" ID="Freemind_Link_945168122" MODIFIED="1224434469033" POSITION="right" TEXT="Exemple">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1224434469414" ID="Freemind_Link_1875194109" MODIFIED="1224434495170" TEXT="Organisation, multi-site, multi-domaine">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1224434495775" ID="Freemind_Link_1972986788" MODIFIED="1224434553772" TEXT="200 personnes, 150 postes par centre diss&#xe9;min&#xe9; partout dans le monde">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1224434619594" ID="Freemind_Link_203736449" MODIFIED="1224434622534" TEXT="Domaine">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1224434623034" ID="Freemind_Link_1997614181" MODIFIED="1224434675048" TEXT="Parfaitement adapt&#xe9; pour les groupement de soci&#xe9;t&#xe9;, permet d&apos;avoir une arborescence fid&#xe8;le au sch&#xe9;ma du groupe : entreprise holding + ses filiales"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1224434775056" ID="Freemind_Link_937407413" MODIFIED="1224434779211" POSITION="right" TEXT="Quels sont les besoins">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1224434780992" ID="Freemind_Link_1800503699" MODIFIED="1224434785179" TEXT="Petite boite">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1224434785496" ID="Freemind_Link_1134622929" MODIFIED="1224434788716" TEXT="Grosse boite">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1224435389444" ID="Freemind_Link_1520128832" MODIFIED="1224435410640" TEXT="Gestion vpn">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1224435413645" ID="Freemind_Link_1404704842" MODIFIED="1224436531532" TEXT="Connexion intersite"/>
+<node COLOR="#111111" CREATED="1224436532008" ID="Freemind_Link_269104343" MODIFIED="1224436536524" TEXT="Gestion des commerciaux"/>
+</node>
+<node COLOR="#990000" CREATED="1224435417763" ID="Freemind_Link_249459584" MODIFIED="1224435437825" TEXT="Gestion clustering">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1224436613363" ID="Freemind_Link_1896884969" MODIFIED="1224436615519" TEXT="But">
+<node COLOR="#111111" CREATED="1224436615867" ID="Freemind_Link_417626919" MODIFIED="1224436628231" TEXT="R&#xe9;parttition de charge"/>
+<node COLOR="#111111" CREATED="1224436628932" ID="Freemind_Link_1277525970" MODIFIED="1224436674209" TEXT="FailOver : tol&#xe9;rance de panne -&gt; reprendre la main si le serveur tombe en panne"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1224435440861" ID="Freemind_Link_613308269" MODIFIED="1224435467378" TEXT="Bureau d&#xe9;port&#xe9;">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1224435471054" ID="Freemind_Link_1886641794" MODIFIED="1224435492610" TEXT="Terminal Server (connexion de l&apos;utilisateur sur un bureau distant"/>
+</node>
+<node COLOR="#990000" CREATED="1224436809659" ID="Freemind_Link_1354027916" MODIFIED="1224436816901" TEXT="Virtualisation de bureau">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1224436851955" ID="Freemind_Link_664580209" MODIFIED="1224436863583" TEXT="Virtualisation du bureau">
+<node COLOR="#111111" CREATED="1224436865139" ID="Freemind_Link_93117707" MODIFIED="1224436881660" TEXT="Sorte de terminal serveur mais avec machine virtuelle sur le serveur"/>
+<node COLOR="#111111" CREATED="1224436901605" ID="Freemind_Link_585208247" MODIFIED="1224436911764" TEXT="Permet d&apos;aller plus loin que le simlpe TSE"/>
+</node>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1224434790640" ID="Freemind_Link_348354384" MODIFIED="1224434792700" TEXT="Commun">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1224434793723" ID="Freemind_Link_1906970057" MODIFIED="1224434801654" TEXT="Messagerie">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1224436943158" ID="Freemind_Link_637630970" MODIFIED="1224436951267" TEXT="Doit &#xea;tre simple d&apos;administration"/>
+</node>
+<node COLOR="#990000" CREATED="1224434802312" ID="Freemind_Link_409719468" LINK="#Freemind_Link_1289602541" MODIFIED="1224435320419" TEXT="Partage de fichier">
+<arrowlink DESTINATION="Freemind_Link_1289602541" ENDARROW="Default" ENDINCLINATION="169;0;" ID="Freemind_Arrow_Link_323720679" STARTARROW="Default" STARTINCLINATION="271;0;"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1224434808593" ID="Freemind_Link_379805131" MODIFIED="1224434812052" TEXT="Gestion de droits">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1224434818858" ID="Freemind_Link_1289602541" MODIFIED="1224435320420" TEXT="Imprimantes">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1224436964486" ID="Freemind_Link_502098067" MODIFIED="1224436993795" TEXT="Pouvoir croiser localisation de l&apos;imprimante avec les droits utilisateurs">
+<node COLOR="#111111" CREATED="1224437000728" ID="Freemind_Link_260613803" MODIFIED="1224437042125" TEXT="Tel classe d&apos;utilsateur &#xe0; le droit d&apos;imprimer, et il doit utiliser telle imprimante si il se trouve &#xe0; tel endroit"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1224437333315" ID="Freemind_Link_1767901165" MODIFIED="1224437415185" TEXT="Messagerie &#xe9;volu&#xe9;">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="messagebox_warning"/>
+<node COLOR="#111111" CREATED="1224437347163" ID="Freemind_Link_1673185912" MODIFIED="1224437349983" TEXT="Mail"/>
+<node COLOR="#111111" CREATED="1224437350347" ID="Freemind_Link_583088354" MODIFIED="1224437356807" TEXT="Calendrier partag&#xe9;"/>
+<node COLOR="#111111" CREATED="1224437357331" ID="Freemind_Link_575323888" MODIFIED="1224437366071" TEXT="Dossiers publics"/>
+<node COLOR="#111111" CREATED="1224437366468" ID="Freemind_Link_1198809766" MODIFIED="1224437373696" TEXT="Carnet d&apos;adresse automatique">
+<node COLOR="#111111" CREATED="1224437378812" ID="Freemind_Link_1496128674" MODIFIED="1224437392704" TEXT="Avec profils dans le carnet d&apos;adresse">
+<node COLOR="#111111" CREATED="1224437396220" ID="Freemind_Link_1023868916" MODIFIED="1224437411366" TEXT="Evolution vs Exchange">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1224437437358" ID="Freemind_Link_1551573296" MODIFIED="1224437461923" TEXT="Acc&#xe8;s distant via web ou smartphone"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1224437162917" ID="Freemind_Link_1312867559" MODIFIED="1224437170577" POSITION="left" TEXT="Probl&#xe9;matique de l&apos;article">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1224437172325" ID="Freemind_Link_1800898981" MODIFIED="1224437196170" TEXT="Pourquoi Microsoft domine le march&#xe9; des postes de travail en entreprise">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1224437208895" ID="Freemind_Link_1915599725" MODIFIED="1224437234715" TEXT="Pourquoi bcp d&apos;admin r&#xe9;seau trouvent compliqu&#xe9; de faire un r&#xe9;seau sous Linux">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1224437567442" ID="Freemind_Link_565210249" MODIFIED="1224437596247" TEXT="Historiquement NT4 et 2000 &#xe9;taient tr&#xe8;s au dessus de la concurrence">
+<node COLOR="#111111" CREATED="1224437603795" ID="Freemind_Link_1033620806" MODIFIED="1224437654217" TEXT="Un non informticien, dans une PME, peut g&#xe9;rer lui m&#xea;me le r&#xe9;seau sans trop de probl&#xe8;me (g&#xe8;re 95 % des cas)"/>
+</node>
+<node COLOR="#111111" CREATED="1224438042729" ID="Freemind_Link_1856682452" MODIFIED="1224438070926" TEXT="Comparer Suse/RedHat/Debian &#xe0; Windows Server"/>
+</node>
+<node COLOR="#990000" CREATED="1224437270112" ID="Freemind_Link_1120668050" MODIFIED="1224437304766" TEXT="=&gt; faudra parler des &#xe9;l&#xe9;ments de contextes : poste client &#xe0; la maison, &#xe9;tudiant, parc logiciel">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+</node>
+</map>
